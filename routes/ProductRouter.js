687e2f38bdf976a88a5cf51a1276c12e2987795e
BedhiefJihed <jihedbndief22@gmail.com>
2022-07-22 15:38:18 +0100

    update with cloudinary

--- routes/ProductRouter.js
+++ routes/ProductRouter.js
@@ -3,58 +3,56 @@ require("express-async-errors");
 require("dotenv").config();
 const router = express.Router();
 const Product = require("../model/Product");
-const multer = require("multer");
 const fs = require("fs");
-const path = require("path");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
-const Upload = require('../utils/upload');
+const Upload = require("../utils/upload");
 const upload = require("../helper/helper").upload;
 const vm = require("v-response");
 const _ = require("underscore");
 
 //create product
-router.post("/add", Upload.array("files",6), async (req, res,next) => {
+router.post("/add", Upload.array("files", 6), async (req, res, next) => {
   if (!req.files || _.isEmpty(req.files)) {
-    return res.status(400)
-        .json(vm.ApiResponse(false, 400, "No file uploaded'"))
-}
-const files = req.files;
-try {
+    return res
+      .status(400)
+      .json(vm.ApiResponse(false, 400, "No file uploaded'"));
+  }
+  const files = req.files;
+  try {
     let urls = [];
     let multiple = async (path) => await upload(path);
     for (const file of files) {
-        const {path} = file;
-        console.log("path", file);
+      const { path } = file;
+      console.log("path", file);
 
-        const newPath = await multiple(path);
-        urls.push(newPath);
-        fs.unlinkSync(path);
+      const newPath = await multiple(path);
+      urls.push(newPath);
+      fs.unlinkSync(path);
     }
     if (urls) {
-        let body = req.body;
-        let bodyw = _.extend(body, {files: urls});
-        let new_Product = new Product(bodyw);
-        await new_Product.save()
-            .then(saved => {
-                return res.json({saved});
-            }).catch(error => {
-                return res.json({msg : error.message});
-            })
-
+      let body = req.body;
+      let bodyw = _.extend(body, { files: urls });
+      let new_Product = new Product(bodyw);
+      await new_Product
+        .save()
+        .then((saved) => {
+          return res.json({ saved });
+        })
+        .catch((error) => {
+          return res.json({ msg: error.message });
+        });
     }
     if (!urls) {
-        return res.status(400)
-            .json(vm.ApiResponse(false, 400, ""))
+      return res.status(400).json(vm.ApiResponse(false, 400, ""));
     }
-
-} catch (e) {
+  } catch (e) {
     console.log("err :", e.message);
     return next(e);
-}
+  }
 });
 
-router.post("/AddProd", Upload.array("files",6), async (req, res) => {
+router.post("/AddProd", Upload.array("files", 6), async (req, res) => {
   let filesArray = [];
   req.files.forEach((element) => {
     const file = {
@@ -121,100 +119,92 @@ router.delete("/delete/:ProductId", async (req, res) => {
 });
 
 //update product
-router.patch("/:ProductId",  Upload.array("files",6), async (req, res) => {
-  
+router.patch("/:ProductId", Upload.array("files", 6), async (req, res) => {
 
-  if(req.files){
+  if (req.files.length > 0) {
     let filesArray = [];
-  req.files.forEach((element) => {
-    const file = {
-      originalname: element.originalname,
+    req.files.forEach((element) => {
+      const file = {
+        originalname: element.originalname,
+      };
+      filesArray.push(file);
+    });
+    var updates = {
+      name: req.body.name,
+      price: req.body.price,
+      sousCategorie: req.body.sousCategorie,
+      rating: req.body.rating,
+      countInStock: req.body.countInStock,
+      files: filesArray,
     };
-    filesArray.push(file);
-  }); 
-    var updates={
-      name: req.body.Name ,
-      price: req.body.Price ,
-      sousCategorie:req.body.SousCategorie,
-      rating:req.body.Rating,
-      countInStock:req.body.CountInStock,
-      files:filesArray,
-    }
-  }else{
-    var updates={
-      name: req.body.Name ,
-      price: req.body.Price ,
-      sousCategorie:req.body.SousCategorie,
-      rating:req.body.Rating,
-      countInStock:req.body.CountInStock,
-    }
+  } else {
+    var updates = {
+      name: req.body.name,
+      price: req.body.price,
+      sousCategorie: req.body.sousCategorie,
+      rating: req.body.rating,
+      countInStock: req.body.countInStock,
+    };
   }
 
   try {
-    const updatedProduct = await Product.findByIdAndUpdate(
+    const updatedProduct = await Product.findOneAndUpdate(
       { _id: req.params.ProductId },
       {
         $set: updates,
-      },
-      { new: true }
+      }
     );
-    res.status(200).json({ msg: "updated" });
-    console.log(updatedProduct);
+    res.status(200).json(updatedProduct);
   } catch (err) {
     console.log(err);
     res.status(400).json({ msg: err.message });
   }
-  // const { Name, Price, SousCategorie, Rating, CountInStock } = req.body;
-  // const product = await Product.findById(req.params.ProductId);
-  // if (product) {
-  //   product.name = Name || product.name;
-  //   product.price = Price || product.price;
-  //   product.sousCategorie = SousCategorie || product.sousCategorie;
-  //   product.rating = Rating || product.rating;
-  //   product.countInStock = CountInStock || product.countInStock;
-  //   if(req.files){
-  //     product.files = filesArray ;
-  //   }
-  //   const updatedProduct = await product.save();
-  //   res.json(updatedProduct);
-  // } else {
-  //   res.status(404);
-  //   throw new Error("Product not found");
-  // }
-  
-
-  
 });
-// async function uploadToCloudinary(locaFilePath) {
-  
-//     // locaFilePath: path of image which was just
-//     // uploaded to "uploads" folder
-  
-//     var mainFolderName = "main";
-//     // filePathOnCloudinary: path of image we want
-//     // to set when it is uploaded to cloudinary
-//     var filePathOnCloudinary = 
-//         mainFolderName + "/" + locaFilePath;
-  
-//     return cloudinary.uploader.upload(locaFilePath, { public_id: filePathOnCloudinary })
-//         .then((result) => {
-  
-//             // Image has been successfully uploaded on
-//             // cloudinary So we dont need local image 
-//             // file anymore
-//             // Remove file from local uploads folder
-//             fs.unlinkSync(locaFilePath);
-  
-//             return {
-//                 message: "Success",
-//                 url: result.url,
-//             };
-//         })
-//         .catch((error) => {
-  
-//             // Remove file from local uploads folder
-//             fs.unlinkSync(locaFilePath);
-//             return { message: "Fail" };
-//         });
-// }
+
+
+//update product cloudinary 
+router.patch("/update/:ProductId", Upload.array("files", 6), async (req, res) => {
+  if (req.files.length > 0) {
+    const files = req.files;
+      let urls = [];
+      let multiple = async (path) => await upload(path);
+      for (const file of files) {
+        const { path } = file;
+        console.log("path", file);
+        const newPath = await multiple(path);
+        urls.push(newPath);
+        fs.unlinkSync(path);
+      }
+      var updates = {
+        name: req.body.name,
+        price: req.body.price,
+        sousCategorie: req.body.sousCategorie,
+        rating: req.body.rating,
+        countInStock: req.body.countInStock,
+        files: urls,
+      }
+      console.log(urls)
+    }else{
+      var updates = {
+        name: req.body.name,
+        price: req.body.price,
+        sousCategorie: req.body.sousCategorie,
+        rating: req.body.rating,
+        countInStock: req.body.countInStock}
+    }
+  try {
+    const updatedProduct = await Product.findOneAndUpdate(
+      { _id: req.params.ProductId },
+      {
+        $set: updates,
+      }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ msg: err.message });
+  }
+});
+
 module.exports = router;
+
