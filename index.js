3d03aa1d371a691c93df1ff2045913a835294b4b
Root99 <azizrezgui4@gmail.com>
2023-05-22 04:57:00 +0100

    changes

--- index.js
+++ index.js
@@ -62,5 +62,5 @@ app.all("*", (req, res, next) => {
   });
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("app working on port " + port + "..."));
