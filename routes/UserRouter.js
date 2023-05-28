b61f2112580f2befe4576978ccf4c74a7f07bc28
Root99 <azizrezgui4@gmail.com>
2023-05-28 06:01:17 +0100

    added forgot password

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -3,7 +3,7 @@ const { body, validationResult } = require("express-validator");
 const router = express.Router();
 const User = require("../model/User");
 const bcrypt = require("bcrypt");
-
+const nodemailer = require("nodemailer");
 
 //Auth
 router.post(
@@ -22,7 +22,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors)
+      return res.status(400).send(errors);
     }
 
     let user = await User.findOne({ email: req.body.email });
@@ -37,7 +37,7 @@ router.post(
       return res.status(404).send("invalid email or password");
     }
     const token = user.generateTokens();
-    return res.status(200).json({user,token});
+    return res.status(200).json({ user, token });
   }
 );
 
@@ -59,7 +59,7 @@ router.post(
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       console.log(errors);
-      return res.status(400).send(errors)
+      return res.status(400).send(errors);
     }
     let user = await User.findOne({ email: req.body.email });
 
@@ -86,14 +86,69 @@ router.post(
 // update product
 router.patch("/updatePassword/:userId", async (req, res) => {
   try {
-    const { userId } = req.params
+    const { userId } = req.params;
     const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt)
-    const userPassword = await User.findByIdAndUpdate({ _id: userId }, { password: hashPassword }, { new: true })
-    return res.status(200).json({ status: true, data: userPassword })
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    const userPassword = await User.findByIdAndUpdate(
+      { _id: userId },
+      { password: hashPassword },
+      { new: true }
+    );
+    return res.status(200).json({ status: true, data: userPassword });
   } catch (error) {
-    return res.status(400).json({ status: false, msg: "error occured! " })
+    return res.status(400).json({ status: false, msg: "error occured! " });
   }
-})
+});
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "azizrezgui4@gmail.com",
+    pass: "waranunohjdwrktt",
+  },
+});
+
+router.post("/forgot", async (req, res) => {
+  const { email } = req.body;
+  const {newPassword} = req.body;
+
+  try {
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    await sendPasswordResetEmail(email, newPassword);
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(newPassword, salt);
+    await user.save();
+
+    res.json({ message: "Password reset email sent" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+
+
+// Helper function to send the password reset email
+async function sendPasswordResetEmail(email, temporaryPassword) {
+  try {
+    await transporter.sendMail({
+      from: "azizrezgui4@gmail.com",
+      to: email,
+      subject: "Password Reset",
+      text: `Your New  password: ${temporaryPassword}`,
+    });
+    console.log("Password reset email sent");
+  } catch (error) {
+    console.error("Error sending password reset email:", error);
+    throw error;
+  }
+}
 
 module.exports = router;
